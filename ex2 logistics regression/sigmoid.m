c986d879efe87065c5afc7f2563fbd335b8a04dc
Jacob Li Peng Cheng <JacobLI@Jacobs-MacBook-Pro-2.local>
2018-08-11 23:55:36 +0800

    change folder name and minor changes to the code

---
+++ ex2 logistics regression/sigmoid.m
@@ -0,0 +1,18 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+% this is mainly to calculate the sigmoid using formula
+g = 1 ./ (e.^(-z)+1);
+
+
+% =============================================================
+
+end
